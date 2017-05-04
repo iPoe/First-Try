16e764c6c423eae7c560e3a27072e4c6b37d4e59
iPoe <patrickgraham@javerianacali.edu.co>
2017-05-04 16:41:56 -0500

    Agregando cosas
    Haciendo una funcion que imprime un menu

--- 1.py
+++ 1.py
@@ -1,1 +1,19 @@
 import tkinter
+import math
+import random
+import time
+
+def Menuoperaciones():
+    """
+    """
+    print(" ====================")
+    print(" MI PROPIO MP3 PLAYER")
+    print(" ====================")
+    print("1)Mostrar la lista de canciones")
+    print("2)Agregar Canción")
+    print("3)Borrar Canción")
+    print("4)Reproducir Canción")
+    print("5)Finalizar")
+    op=0
+
+
