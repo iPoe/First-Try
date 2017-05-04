468cba432cf3165ff05881891f03aca4df61e77b
iPoe <patrickgraham@javerianacali.edu.co>
2017-05-04 17:08:39 -0500

    Haciendo un ciclo
    Esto es para que la persona pueda elegir una opcion

--- 1.py
+++ 1.py
@@ -16,4 +16,42 @@ def Menuoperaciones():
     print("5)Finalizar")
     op=0
 
+    while(op <= 1 or op>=5):
+            op= eval(input("Ingrese una opcion: "))
+                     
+            if(op >= 1 and op <=5):
+                
+                if(op == 1):
+                    MostrarListaCanciones(Listacanciones,Listaduraciones)
+                if(op == 2):
+                    nombre=str(input("Name Song? "))
+                    duracion= int(input("Duration? "))
+                    Agregarcancion(nombre,duracion,Listacanciones,Listaduraciones)
+                    Menuoperaciones()
+                if(op == 3):
+                    print(str(Listacanciones))                
+                    posicion = int(input("What song u want to delete.? "))
+                    Borrarcancion(posicion, Listacanciones, Listaduraciones)
+                    Menuoperaciones()                
+                if(op == 4):
+                    print(str(Listacanciones))                
+                    posicion=int(input("What song u want to play.? "))
+                    Reproducircancion(posicion, Listacanciones, Listaduraciones)
+                    Menuoperaciones()
+                if(op == 5):
+                    exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
 
